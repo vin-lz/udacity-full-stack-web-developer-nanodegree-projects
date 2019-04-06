a4d01be0bbf655b0beb81873c2168d3d161be468
Zhuo Liu <vin_lz@outlook.com>
2019-04-06 16:53:54 -0400

    feat: List popularity of authors

--- FSND-Virtual-Machine/vagrant/logs_analysis/logs_analysis.py
+++ FSND-Virtual-Machine/vagrant/logs_analysis/logs_analysis.py
@@ -10,10 +10,13 @@ import psycopg2
 DBNAME="news"
 
 def get_popular_articles():
-    """Return the most popular articles with numbers of views from 'news', descendent order."""
+    """Return the most popular articles with numbers of views from 'news', descending order."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("SELECT articles.title, COUNT(*) AS num FROM articles, log WHERE log.path LIKE '%' || articles.slug GROUP BY articles.title ORDER BY num DESC LIMIT 3;")
+    c.execute("""SELECT articles.title, COUNT(*) AS num FROM articles, log 
+        WHERE log.path LIKE '%' || articles.slug 
+        GROUP BY articles.title 
+        ORDER BY num DESC LIMIT 3;""")
     popular_articles = c.fetchall()
     db.close()
     print("===================================================")
@@ -23,4 +26,21 @@ def get_popular_articles():
     return
 
 
-get_popular_articles()
+def get_popular_authors():
+    """Return the most popular authors with numbers of views from 'news', descending order."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("""SELECT authors.name, SUM(popular_articles.num) AS t_num FROM (SELECT articles.title, COUNT(*) AS num FROM articles, log WHERE log.path LIKE '%' || articles.slug 
+        GROUP BY articles.title) as popular_articles, articles, authors 
+        WHERE popular_articles.title=articles.title AND authors.id=articles.author 
+        GROUP BY authors.name 
+        ORDER BY t_num DESC;""")
+    popular_articles = c.fetchall()
+    db.close()
+    print("===================================================")
+    print("2. Who are the most popular article authors of all time?")
+    for item in popular_articles:
+        print(u"\u2022 " + item[0] + " — " + str(item[1]) + " views")
+    return
+
+get_popular_authors()
