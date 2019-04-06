54b264e1ac102f988fb48e62f98cee1568de7071
Zhuo Liu <vin_lz@outlook.com>
2019-04-06 15:48:17 -0400

    feat: Minor text change

--- FSND-Virtual-Machine/vagrant/logs_analysis/logs_analysis.py
+++ FSND-Virtual-Machine/vagrant/logs_analysis/logs_analysis.py
@@ -10,15 +10,15 @@ import psycopg2
 DBNAME="news"
 
 def get_popular_articles():
-    """Return all posts from the 'database', most recent first."""
+    """Return the most popular articles with numbers of views from 'news', descendent order."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute("SELECT path, COUNT(*) AS num FROM log WHERE status='200 OK' AND path LIKE '/article/%' GROUP BY path ORDER BY num DESC LIMIT 3;")
-    populer_articles = c.fetchall()
+    popular_articles = c.fetchall()
     db.close()
     print("===================================================")
     print("1. What are the most popular three articles of all time?")
-    for item in populer_articles:
+    for item in popular_articles:
         print(u"\u2022 " + item[0][9:].replace("-", " ").title() + " — " + str(item[1]) + " views")
     return
 
