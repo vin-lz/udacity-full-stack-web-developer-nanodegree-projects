84370e92f08a39c07dc7a653a11419dccc61f5e4
Zhuo Liu <vin_lz@outlook.com>
2019-04-06 16:19:11 -0400

    feat: Use pattern matching to join table articles and log

--- FSND-Virtual-Machine/vagrant/logs_analysis/logs_analysis.py
+++ FSND-Virtual-Machine/vagrant/logs_analysis/logs_analysis.py
@@ -13,13 +13,13 @@ def get_popular_articles():
     """Return the most popular articles with numbers of views from 'news', descendent order."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("SELECT path, COUNT(*) AS num FROM log WHERE status='200 OK' AND path LIKE '/article/%' GROUP BY path ORDER BY num DESC LIMIT 3;")
+    c.execute("SELECT articles.title, COUNT(*) AS num FROM articles, log WHERE log.path LIKE '%' || articles.slug GROUP BY articles.title ORDER BY num DESC LIMIT 3;")
     popular_articles = c.fetchall()
     db.close()
     print("===================================================")
     print("1. What are the most popular three articles of all time?")
     for item in popular_articles:
-        print(u"\u2022 " + item[0][9:].replace("-", " ").title() + " — " + str(item[1]) + " views")
+        print(u"\u2022 " + item[0].title() + " — " + str(item[1]) + " views")
     return
 
 
