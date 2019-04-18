a5ebbde494823de611f9c43a3c77a4dc01b7e456
Zhuo Liu <vin_lz@outlook.com>
2019-04-17 23:01:40 -0400

    feat: Enable DELTE menu item

--- fullstack-nanodegree-vm-master/vagrant/project.py
+++ fullstack-nanodegree-vm-master/vagrant/project.py
@@ -49,9 +49,18 @@ def editMenuItem(restaurant_id, menu_id):
 
 # Task 3: Create a route for deleteMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/', methods = ['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    item = session.query(MenuItem).filter_by(restaurant_id=restaurant_id, id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('deleteMenuItem.html', 
+        restaurant_id = restaurant_id, menu_id = menu_id, item = item)
 
 if __name__ == '__main__':
     app.debug = True
